fb4c46d870ca45543d898f298d20452f2c4b5fc1
Muhammet Topcu <muhammetopcu06@gmail.com>
2026-03-11 15:36:49 +0300

    Challenge done

---
+++ src/main/java/org/example/model/Lamp.java
@@ -0,0 +1,33 @@
+package org.example.model;
+
+import org.example.model.enums.LampType;
+
+public class Lamp {
+    private LampType style;
+    private boolean battery;
+    private int globRating;
+
+    public Lamp(LampType style, boolean batter, int globRating) {
+        this.style = style;
+        this.battery = batter;
+        this.globRating = globRating;
+    }
+
+    public LampType getStyle() {
+        return style;
+    }
+
+    public boolean isBattery() {
+        return battery;
+    }
+
+    public int getGlobRating() {
+        return globRating;
+    }
+
+    public void turnOn(){
+        System.out.println("Lamp is being turned on.");
+    }
+
+
+}
